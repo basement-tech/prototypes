5df98e8e52bddd682b3592642cb591fd2900bf28
djzman078 <djzman078@gmail.com>
2025-08-31 18:43:19 -0500

    dynamic header based on header size

--- NeoFileCreator/neofilecreator.c
+++ NeoFileCreator/neofilecreator.c
@@ -7,6 +7,8 @@
 #define DELIM_START '<'
 #define DELIM_STOP  '>'
 
+#define MAX_HDR_SIZE 1024
+
 static const char header_json[] =
 R"==(
 {"strategy" : "bitwise", "__comment" : "first line is skipped after noting strategy"}
@@ -18,7 +20,8 @@ R"==(
 }
 )==";
 
-char *header = "{\"label\":\"USER-5\", }";
+char header[MAX_HDR_SIZE] = {0};;
+
 //                       R           G           B           W        T
 uint32_t data[] = { 0x10101010, 0x00000000, 0x00000000, 0x00000000, 1000,
                     0x01010101, 0x00000000, 0x00000000, 0x00000000, 1000,
@@ -28,8 +31,9 @@ void main(int argc, char **argv)  {
     char filename[128];
     char label[32] = "USER-5";
     char strategy[32] = "bitwise";
-    char bonus[] = "\"bonus\" : { \"depth\" : \"2\", \"pixel_cnt\" : 32, \"brightness\" : {\"r\": 255,  \"g\": 0, \"b\": 0, \"w\": 0}}";
+    char bonus[] = "{ \"depth\" : \"2\", \"pixel_cnt\" : 32, \"brightness\" : {\"r\": 255,  \"g\": 0, \"b\": 0, \"w\": 0}}";
     char pixelcnt[32] = "32";
+    char *hdr_len_label = "  \"hdrlen\" : ";
     int jlen = 0; // length of the json to follow
 
     FILE *fp;
@@ -59,12 +63,18 @@ void main(int argc, char **argv)  {
             fprintf(stderr, "Error: can't open file %s\n", filename);
         else
         {
-            fprintf(fp, "{\"filetype\" : \"BIN_BW\", \"__comment\" : \"points as binary\"}\n");
-            fprintf(fp, "{");
-            fprintf(fp, "\"label\" : \"%s\",", label);
-            fprintf(fp, "\"strategy\" : \"%s\",", strategy);
-            fprintf(fp, "\"bonus\" : \"%s\",", bonus);
-            fprintf(fp, "\"__comment\" : \"first line is skipped\"}\n");
+            uint32_t cbal = sizeof(header);
+            snprintf(header, cbal, "{\"filetype\" : \"BIN_BW\", \"__comment\" : \"points as binary\"}\n");
+            snprintf(header+strlen(header), (cbal-=strlen(header)), "{\n");
+            snprintf(header+strlen(header), (cbal-=strlen(header)), "  \"label\" : \"%s\",\n", label);
+            snprintf(header+strlen(header), (cbal-=strlen(header)), "  \"strategy\" : \"%s\",\n", strategy);
+            snprintf(header+strlen(header), (cbal-=strlen(header)), "  \"bonus\" : %s,\n", bonus);
+            snprintf(header+strlen(header), (cbal-=strlen(header)), "  \"__comment\" : \"back and forth\",\n");
+
+            uint32_t hdr_len = strlen(header);
+            snprintf(header+strlen(header), (cbal-=strlen(header)), "%s%4u\n}\n", hdr_len_label, hdr_len+strlen(hdr_len_label)+3);
+
+            fprintf(stdout, "%s", header);
             //printf("wrote size of header as %d\n", strlen(header_json));
             //num = fwrite(header_json, sizeof(uint8_t), strlen(header_json), fp);
             //printf("%d characters written\n", num);
