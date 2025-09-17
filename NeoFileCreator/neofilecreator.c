4859c349137cdfb299e92d7339d4db808c04909f
djzman078 <djzman078@gmail.com>
2025-09-16 20:28:09 -0500

    working pong file creation w/ 65 pixels

--- NeoFileCreator/neofilecreator.c
+++ NeoFileCreator/neofilecreator.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define NUM_ARGS 6  //
+#define NUM_ARGS 8  //
 #define MIN_ARGS 0  //
 #define DELIM_START '<'
 #define DELIM_STOP  '>'
@@ -64,9 +64,11 @@ void main(int argc, char **argv)  {
     char fileopt[32] = "default";
     char bonus[128] = "{ \"depth\" : 2, \"pixel_cnt\" : 64, \"brightness\" : {\"r\": 64,  \"g\": 64, \"b\": 64, \"w\": 0}}";
     char pixelcnt[32] = "64";
+    int32_t interval = 1000;  // interval between points
+    int ipixelcnt = 64;
     int idepth = 2;  // depth as integer
     int jlen = 0; // length of the json to follow
-    uint32_t cbal;  // character balance when creating character strings
+    int32_t cbal;  // character balance when creating character strings
 
     FILE *fp;
     int num = 0;
@@ -91,6 +93,7 @@ void main(int argc, char **argv)  {
                         case 'f':  // filename
                         case 'p':  // pixelcount
                         case 'o':  // file contents option
+                        case 't':  // time interval
                             arg = (*argv)[1];
                             break;
 
@@ -113,8 +116,12 @@ void main(int argc, char **argv)  {
                         strncpy(pixelcnt, *argv, sizeof(pixelcnt));
                         break;
 
-                    case 'o':  // strategy
+                    case 'o':  // file content option
                         strncpy(fileopt, *argv, sizeof(fileopt));
+                        break;
+
+                    case 't':  // time interval
+                        interval = atoi(*argv);
                         break;
 
                     default:
@@ -127,33 +134,31 @@ void main(int argc, char **argv)  {
             }
         }
         printf("Using:\n");
-        printf("   filename :  %s\n", filename);
-        printf("   pixelcount: %s\n", pixelcnt);
-        printf("   fileopt:    %s\n", fileopt);
+        printf("   filename :  \"%s\"\n", filename);
+        printf("   pixelcount: \"%s\"\n", pixelcnt);
+        printf("   fileopt:    \"%s\"\n", fileopt);
 
-        if(err >=0)  {
+        if(err >= 0)  {
             if((fp = fopen(filename, "wb")) == NULL)
                 fprintf(stderr, "Error: can't open file %s\n", filename);
             else
             {
-                idepth = atoi(pixelcnt) / BITS_PER_BITMAP;  // remainder truncated
-                if((atoi(pixelcnt) % BITS_PER_BITMAP) > 0)  // leftovers require a full depth added
+                ipixelcnt = atoi(pixelcnt);
+                idepth = ipixelcnt / BITS_PER_BITMAP;  // remainder truncated
+                if((ipixelcnt % BITS_PER_BITMAP) > 0)  // leftovers require a full depth added
                     idepth++;
                 printf("   calculated depth:  %d\n", idepth);
 
                 /*
                  * create the bonus string based on parameters
                  */
-                strncpy(bonus,
-                        "{ \"depth\" : 2, \"pixel_cnt\" : 64, \"brightness\" : {\"r\": 64,  \"g\": 64, \"b\": 64, \"w\": 0}}",
-                        sizeof(bonus));
-
-                cbal - sizeof(bonus);
+                cbal = sizeof(bonus);
                 snprintf(bonus, cbal, "{ \"depth\" : ");
-                snprintf(bonus+strlen(bonus), (cbal-=strlen(bonus)), "%2d, ", idepth);
-                snprintf(bonus+strlen(bonus), (cbal-=strlen(bonus)), "\"pixel_cnt\" : ");
-                snprintf(bonus+strlen(bonus), (cbal-=strlen(bonus)), "%s, ", pixelcnt);
-                snprintf(bonus+strlen(bonus), (cbal-=strlen(bonus)), "\"brightness\" : {\"r\": 64,  \"g\": 64, \"b\": 64, \"w\": 0}}");
+                snprintf(bonus+strlen(bonus), (cbal-strlen(bonus)), "%2d, ", idepth);
+                snprintf(bonus+strlen(bonus), (cbal-strlen(bonus)), "\"pixel_cnt\" : ");
+                snprintf(bonus+strlen(bonus), (cbal-strlen(bonus)), "%3d, ", ipixelcnt);
+                snprintf(bonus+strlen(bonus), (cbal-strlen(bonus)), "\"brightness\" : {\"r\": 64,  \"g\": 64, \"b\": 64, \"w\": 0}}");
+                printf("generated bonus string: %s\n", bonus);
                 
                 /*
                  * write the part that is common to all file content options
@@ -161,11 +166,11 @@ void main(int argc, char **argv)  {
                 cbal = sizeof(header);  // balance is full buffer at this point
 
                 snprintf(header, cbal, "{\n");
-                snprintf(header+strlen(header), (cbal-=strlen(header)), "  \"label\" : \"%s\",\n", label);
-                snprintf(header+strlen(header), (cbal-=strlen(header)), "  \"strategy\" : \"%s\",\n", strategy);
-                snprintf(header+strlen(header), (cbal-=strlen(header)), "  \"bonus\" : %s,\n", bonus);
-                snprintf(header+strlen(header), (cbal-=strlen(header)), "  \"__comment\" : \"back and forth\"\n");
-                snprintf(header+strlen(header), (cbal-=strlen(header)), "}\n");
+                snprintf(header+strlen(header), (cbal-strlen(header)), "  \"label\" : \"%s\",\n", label);
+                snprintf(header+strlen(header), (cbal-strlen(header)), "  \"strategy\" : \"%s\",\n", strategy);
+                snprintf(header+strlen(header), (cbal-strlen(header)), "  \"bonus\" : %s,\n", bonus);
+                snprintf(header+strlen(header), (cbal-strlen(header)), "  \"__comment\" : \"back and forth\"\n");
+                snprintf(header+strlen(header), (cbal-strlen(header)), "}\n");
 
                 uint32_t hdr_len = strlen(header);
                 printf("Header as buffer:\n%s\n", header);
@@ -186,20 +191,81 @@ void main(int argc, char **argv)  {
                  * the original test data
                  * NOTE: relies on canned data above and initialized values
                  */
-                if(strcmp("default", fileopt) == 0)  {
+                if(strncmp("default", fileopt, strlen("default")) == 0)  {
 
                     printf("sizeof() each binary data structure = %d\n", sizeof(seq_bin_t));
                     num = fwrite(data, sizeof(uint8_t), sizeof(data), fp);
                     printf("%d binary bytes written\n", num);
                 }
-                else if(strcmp("pong", fileopt) == 0)  {
-                    
+
+                /*
+                 * single pixel moving back and forth
+                 * all colors equal at this point
+                 */
+                else if(strncmp("pong", fileopt, strlen("pong")) == 0)  {
+                    uint32_t mask = 0x01;
+                    int depth = 0;
+                    int actd = -1;  // depth level that is being lit
+                    int point = 0;                    
+                    seq_bin_t bindata;
+
+                    /*
+                     * in this case, the number of pixels is a sarogate 
+                     * for the number of points that we'll have in the sequence
+                     * to completely traverse the strand
+                     */
+                    for(point = 0; point < ipixelcnt; point++)  {
+                        // first point in the next depth level
+                        if((point % (BITS_PER_BITMAP-1)) == 0)  {
+                            actd++;
+                            mask = 0x01;
+                        }
+                        for(depth = 0; depth < idepth; depth++)  {
+                            bindata.o = depth;
+                            bindata.s = 0;  // no servo movement
+                            bindata.a = 0;  // no servo movement
+                            bindata.d = interval;  // 1000 mS by default
+
+                            if(depth == actd)  {
+                                bindata.r = mask;
+                                bindata.g = mask;
+                                bindata.b = mask;
+                                bindata.w = mask;
+                            }
+                            else  {
+                                bindata.r = 0;
+                                bindata.g = 0;
+                                bindata.b = 0;
+                                bindata.w = 0;
+                            }
+                            fwrite(&bindata, sizeof(uint8_t), sizeof(bindata), fp);
+                        }
+                        mask = mask << 1;
+                    }
+                    /*
+                     * write the termination point
+                     */
+                    for(depth = 0; depth < idepth; depth++)  {
+                        bindata.o = depth;
+                        bindata.s = 0;  // no servo movement
+                        bindata.a = 0;  // no servo movement
+                        bindata.d = -1;  // termination value
+
+                        bindata.r = 0;
+                        bindata.g = 0;
+                        bindata.b = 0;
+                        bindata.w = 0;
+
+                        fwrite(&bindata, sizeof(uint8_t), sizeof(bindata), fp);
+                    }
                 }
                 else
-                    fprintf(stderr, "Error: bad file contents option (file empty)");
+                    fprintf(stderr, "Error: bad file contents option (\"%s\")", fileopt);
 
                 fclose(fp);
             }
         }
+        else
+            fprintf(stderr, "USAGE: neofilecreator.exe -p <numpixels> -o <defaule, pong> -f <filename>\n");
     }
 }
